d11b82d7fc76f4e8275c34d1e4ed5271fce7d260
Adnan Ali <adnan.ali@gmail.com>
2022-08-03 10:56:43 -0400

    Adding indent config.

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,44 +4,51 @@ vim.opt.termguicolors = true
 
 vim.opt.mouse = "a"
 
+-- indent.
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.cmd [[set list listchars=tab:▸\ ,trail:·]]
+
 -- plugins.
 vim.cmd [[packadd packer.nvim]]
 vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost init.lua source <afile> | PackerCompile
-  augroup end
+augroup packer_user_config
+	autocmd!
+	autocmd BufWritePost init.lua source <afile> | PackerCompile
+augroup end
 ]])
 
 require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+	use 'wbthomason/packer.nvim'
 
-  use {
-	  'rmehri01/onenord.nvim',
-	  config = function()
-		  require('onenord').setup()
-	  end
-  }
+	use {
+		'rmehri01/onenord.nvim',
+		config = function()
+			require('onenord').setup()
+		end
+	}
 
-  use {
-	  'nvim-lualine/lualine.nvim',
-	  requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-	  config = function()
-		  require('lualine').setup()
-	  end
-  }
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+		config = function()
+			require('lualine').setup()
+		end
+	}
 
-  use {
-	  'nvim-treesitter/nvim-treesitter',
-	  run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-	  config = function()
-		  require'nvim-treesitter.configs'.setup {
-			  auto_install = true,
-			  highlight = {
-				  enable = true,
-			  }
-		  }
-	  end
-  }
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+		config = function()
+			require'nvim-treesitter.configs'.setup {
+				auto_install = true,
+				highlight = {
+					enable = true,
+				}
+			}
+		end
+	}
 end)
 
