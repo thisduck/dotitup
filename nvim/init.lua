60b89ba766c733b7e9ed245bf46a45dcbeed7565
Adnan Ali <adnan.ali@gmail.com>
2022-08-03 09:09:13 -0400

    Adding autocommand to source init.lua.

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,6 +6,13 @@ vim.opt.mouse = "a"
 
 -- plugins.
 vim.cmd [[packadd packer.nvim]]
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost init.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
