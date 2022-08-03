674dddaceb360674194cdf3bf520bfac0c1b3700
Adnan Ali <adnan.ali@gmail.com>
2022-08-03 09:02:23 -0400

    Configuring packer.

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,9 @@ vim.opt.cursorline = true
 vim.opt.termguicolors = true
 
 vim.opt.mouse = "a"
+
+-- plugins.
+vim.cmd [[packadd packer.nvim]]
+require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+end)
