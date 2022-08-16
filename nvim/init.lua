c68fc64c8c189222004d5d608bc9b01725e67345
Adnan Ali <adnan.ali@gmail.com>
2022-08-15 22:38:14 -0400

    Adding highlightedyank.

--- nvim/init.lua
+++ nvim/init.lua
@@ -50,6 +50,7 @@ require('packer').startup(function(use)
 		'rmehri01/onenord.nvim',
 		config = function()
 			require('onenord').setup()
+			vim.cmd[[ highlight IncSearch gui=bold ]]
 		end
 	}
 	
@@ -108,4 +109,6 @@ require('packer').startup(function(use)
 	}
 
 	use "tversteeg/registers.nvim"
+
+	use "machakann/vim-highlightedyank"
 end)
