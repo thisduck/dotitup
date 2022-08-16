cdd72c33f77d5efb78ec04df0f16cceeaa28ec9c
Adnan Ali <adnan.ali@gmail.com>
2022-08-15 22:25:49 -0400

    Adding onenord.

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,5 +7,12 @@ vim.opt.mouse = 'a'
 -- plugins.
 vim.cmd [[packadd packer.nvim]]
 require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+	use 'wbthomason/packer.nvim'
+
+	use {
+	  'rmehri01/onenord.nvim',
+	  config = function()
+	    require('onenord').setup()
+	  end
+	}
 end)
