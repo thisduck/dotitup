a356b98350e945a51b23aba01a9b0fd022dabef6
Adnan Ali <adnan.ali@gmail.com>
2022-08-15 22:30:07 -0400

    Adding indent config.

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,13 @@ vim.opt.termguicolors = true
 
 vim.opt.mouse = 'a'
 
+-- indent.
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.cmd [[set list listchars=tab:▸\ ,trail:·]]
+
 -- plugins.
 vim.cmd [[packadd packer.nvim]]
 
