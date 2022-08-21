bd011b3f8448295bb5cfc8077c0af03d49e0a40c
Adnan Ali <adnan.ali@gmail.com>
2022-08-21 00:47:04 -0400

    Adding nvim-cmp plugin.

--- nvim/init.lua
+++ nvim/init.lua
@@ -444,4 +444,136 @@ require("packer").startup(function(use)
       }
     end,
   }
+
+  use {
+    "hrsh7th/nvim-cmp",
+    requires = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "lukas-reineke/cmp-rg",
+      "quangnguyen30192/cmp-nvim-tags",
+      "andersevenrud/cmp-tmux",
+      "petertriho/cmp-git",
+
+      "L3MON4D3/LuaSnip",
+      "rafamadriz/friendly-snippets",
+      "saadparwaiz1/cmp_luasnip",
+
+      "onsails/lspkind.nvim",
+    },
+    config = function()
+      vim.cmd [[set completeopt=menu,menuone,noselect]]
+
+      local cmp = require "cmp"
+      if not cmp then
+        return
+      end
+
+      local luasnip = require "luasnip"
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+      end
+
+      cmp.setup {
+        completion = {
+          keyword_length = 3,
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.menu = string.format("[%s]", entry.source.name)
+            return vim_item
+          end,
+        },
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert {
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.confirm { select = true }
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        },
+        sources = cmp.config.sources {
+          { name = "nvim_lsp", priority = 100 },
+          { name = "luasnip", priority = 100, keyword_length = 2 },
+          { name = "nvim_lsp_signature_help" },
+          {
+            name = "buffer",
+            option = {
+              get_bufnrs = function()
+                return vim.api.nvim_list_bufs()
+              end,
+            },
+          },
+          { name = "path" },
+          { name = "tags" },
+          { name = "treesitter" },
+          { name = "rg", option = { pattern = [[[\w_-]{5,60}]] } },
+          { name = "tmux", max_item_count = 10, option = { all_panes = true } },
+        },
+      }
+
+      require("cmp_git").setup()
+      cmp.setup.filetype("gitcommit", {
+        sources = cmp.config.sources {
+          { name = "git" },
+          { name = "buffer" },
+        },
+      })
+
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+          { name = "tags" },
+          { name = "buffer" },
+          { name = "rg", option = { pattern = [[[\w_-]{5,60}]] } },
+        }),
+      })
+
+      require("luasnip").filetype_extend("ruby", { "rails" })
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  }
 end)
