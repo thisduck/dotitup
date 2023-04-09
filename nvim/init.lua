01345e9f966590cf7afb9229fd36f7e85d63e191
Adnan Ali <adnan.ali@gmail.com>
2023-04-09 16:43:16 -0400

    Adding yanky plugin.

--- nvim/init.lua
+++ nvim/init.lua
@@ -112,5 +112,21 @@ require("lazy").setup({
       vim.o.timeoutlen = 300
       require("which-key").setup({})
     end,
+  },
+  {
+    "gbprod/yanky.nvim",
+    config = function()
+      require("yanky").setup({})
+
+      vim.keymap.set({"n","x"}, "p", "<Plug>(YankyPutAfter)")
+      vim.keymap.set({"n","x"}, "P", "<Plug>(YankyPutBefore)")
+      vim.keymap.set({"n","x"}, "gp", "<Plug>(YankyGPutAfter)")
+      vim.keymap.set({"n","x"}, "gP", "<Plug>(YankyGPutBefore)")
+
+      vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)")
+      vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)")
+
+      vim.keymap.set({"n","x"}, "y", "<Plug>(YankyYank)")
+    end
   }
 })
