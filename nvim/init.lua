73737c17568f83ff1f75bf7a5eef05db09a597d0
Adnan Ali <adnan.ali@gmail.com>
2023-04-09 16:11:05 -0400

    Adding lazy.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,20 @@ vim.opt.cursorline = true
 vim.opt.termguicolors = true
 
 vim.opt.mouse = "a"
+
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- plugins.
+require("lazy").setup({})
