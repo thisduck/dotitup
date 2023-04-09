c8f1971d576db6334c04924b5bfaf6eeaa639f46
Adnan Ali <adnan.ali@gmail.com>
2023-04-09 16:24:54 -0400

    Adding indenting settings.

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,14 @@ vim.opt.termguicolors = true
 
 vim.opt.mouse = "a"
 
+-- indent.
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.cmd [[set list listchars=tab:▸\ ,trail:·]]
+
 -- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -27,22 +35,22 @@ require("lazy").setup({
     end
   },
   {
-	  'nvim-lualine/lualine.nvim',
-	  dependencies = { 'kyazdani42/nvim-web-devicons' },
-	  config = function()
-		  require('lualine').setup()
-	  end
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'kyazdani42/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup()
+    end
   },
   {
-	  'nvim-treesitter/nvim-treesitter',
-	  build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-	  config = function()
-		  require'nvim-treesitter.configs'.setup {
-			  auto_install = true,
-			  highlight = {
-				  enable = true,
-			  }
-		  }
-	  end
+    'nvim-treesitter/nvim-treesitter',
+    build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        auto_install = true,
+        highlight = {
+          enable = true,
+        }
+      }
+    end
   }
 })
