cf7409d479a6f556a7e96f9dc7df97c77714264a
Adnan Ali <adnan.ali@gmail.com>
2023-04-09 17:28:42 -0400

    Adding signcolumn and basic lsp.

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,6 +43,9 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.keymap.set('n', 'g/', '<cmd>nohlsearch<cr>')
 
+-- sign column.
+vim.opt.signcolumn = "yes"
+
 -- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -321,4 +324,31 @@ require("lazy").setup({
     end,
   },
   "tpope/vim-commentary",
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      require'lspconfig'.lua_ls.setup {
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+              version = 'LuaJIT',
+            },
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = {'vim'},
+            },
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      }
+    end
+  }
 })
