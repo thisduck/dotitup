8047740c37264209698a80c9194b65104b26a4a5
Adnan Ali <adnan.ali@gmail.com>
2023-04-09 16:44:29 -0400

    Adding substitute plugin.

--- nvim/init.lua
+++ nvim/init.lua
@@ -128,5 +128,25 @@ require("lazy").setup({
 
       vim.keymap.set({"n","x"}, "y", "<Plug>(YankyYank)")
     end
+  },
+  {
+    "gbprod/substitute.nvim",
+    config = function()
+      require("substitute").setup({
+        on_substitute = function(event)
+          require("yanky").init_ring("p", event.register, event.count, event.vmode:match("[vV�]"))
+        end,
+      })
+
+      vim.keymap.set("n", "s", "<cmd>lua require('substitute').operator()<cr>", { noremap = true })
+      vim.keymap.set("n", "ss", "<cmd>lua require('substitute').line()<cr>", { noremap = true })
+      vim.keymap.set("n", "S", "<cmd>lua require('substitute').eol()<cr>", { noremap = true })
+      vim.keymap.set("x", "s", "<cmd>lua require('substitute').visual()<cr>", { noremap = true })
+
+      vim.keymap.set("n", "sx", "<cmd>lua require('substitute.exchange').operator()<cr>", { noremap = true })
+      vim.keymap.set("n", "sxx", "<cmd>lua require('substitute.exchange').line()<cr>", { noremap = true })
+      vim.keymap.set("x", "X", "<cmd>lua require('substitute.exchange').visual()<cr>", { noremap = true })
+      vim.keymap.set("n", "sxc", "<cmd>lua require('substitute.exchange').cancel()<cr>", { noremap = true })
+    end
   }
 })
