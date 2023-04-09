8f01572de1ff7ca810b0c6f42baaf2eb69c38d3f
Adnan Ali <adnan.ali@gmail.com>
2023-04-09 16:24:02 -0400

    Adding treesitter.

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,5 +32,17 @@ require("lazy").setup({
 	  config = function()
 		  require('lualine').setup()
 	  end
+  },
+  {
+	  'nvim-treesitter/nvim-treesitter',
+	  build = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+	  config = function()
+		  require'nvim-treesitter.configs'.setup {
+			  auto_install = true,
+			  highlight = {
+				  enable = true,
+			  }
+		  }
+	  end
   }
 })
