327ce0c58435eb51d87dc900aeecd03ecba4fda4
Adnan Ali <adnan.ali@gmail.com>
2023-04-09 21:40:39 -0400

    Adding nvim-cmp.

--- nvim/init.lua
+++ nvim/init.lua
@@ -256,11 +256,13 @@ require("lazy").setup({
       vim.keymap.set('n', '<leader>fh', [[<cmd>Telescope help_tags<cr>]], { desc = "Help tags" })
       vim.keymap.set('n', '<leader>fr', [[<cmd>Telescope resume<cr>]], { desc = "Resume last search" })
       vim.keymap.set('n', 'K', [[<cmd>Telescope grep_string<cr>]], { desc = "Search word under cursor" })
-      vim.keymap.set('n', '<leader>/',
-        [[<cmd>lua require'telescope.builtin'.grep_string({ search = vim.fn.input("Search: ") })<cr>]],
-        { desc = "Search word under cursor" })
+      vim.keymap.set('n', '<leader>/', [[:Search<space>]], { desc = "Search in project" })
 
       vim.cmd [[ autocmd User TelescopePreviewerLoaded setlocal wrap ]]
+
+      vim.api.nvim_create_user_command("Search", function(opts)
+        require("telescope.builtin").grep_string { search = opts.args }
+      end, { nargs = 1 })
     end
   },
   {
@@ -485,6 +487,166 @@ require("lazy").setup({
     "stevearc/dressing.nvim",
     config = function()
       require("dressing").setup {}
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "lukas-reineke/cmp-rg",
+      "quangnguyen30192/cmp-nvim-tags",
+      "andersevenrud/cmp-tmux",
+      "petertriho/cmp-git",
+
+      "L3MON4D3/LuaSnip",
+      "rafamadriz/friendly-snippets",
+      "saadparwaiz1/cmp_luasnip",
+      {
+        "zbirenbaum/copilot.lua",
+        config = function()
+          require("copilot").setup({
+            suggestion = {
+              enabled = true,
+              auto_trigger = true,
+              debounce = 75,
+              keymap = {
+                accept = "<C-l>",
+                accept_word = false,
+                accept_line = false,
+                next = "<C-;>",
+                prev = "<C-j>",
+                dismiss = "<C-]>",
+              },
+            },
+          })
+        end,
+      },
+      { "zbirenbaum/copilot-cmp", after = { "copilot.lua" }, opts = {} },
+    },
+    config = function()
+      vim.cmd [[set completeopt=menu,menuone,noselect]]
+
+      local cmp = require "cmp.init"
+      if not cmp then
+        return
+      end
+
+      local luasnip = require "luasnip"
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+      end
+
+      cmp.setup {
+        completion = {
+          keyword_length = 2,
+        },
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.menu = string.format("[%s]", entry.source.name)
+            return vim_item
+          end,
+        },
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert {
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+              -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+              -- they way you will only jump inside the snippet region
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        },
+        sources = cmp.config.sources {
+          { name = "copilot",                priority = 99 },
+          { name = "nvim_lsp",               priority = 100 },
+          { name = "luasnip",                priority = 100 },
+          { name = "nvim_lsp_signature_help" },
+          {
+            name = "buffer",
+            option = {
+              get_bufnrs = function()
+                return vim.api.nvim_list_bufs()
+              end,
+            },
+          },
+          { name = "path" },
+          { name = "tags" },
+          { name = "treesitter" },
+          { name = "rg",        option = { pattern = [[[\w_-]{5,60}]] } },
+          { name = "tmux",      max_item_count = 10,                    option = { all_panes = true } },
+        },
+      }
+
+      require("cmp_git").setup()
+      cmp.setup.filetype("gitcommit", {
+        sources = cmp.config.sources {
+          { name = "git" },
+          {
+            name = "buffer",
+            option = {
+              get_bufnrs = function()
+                return vim.api.nvim_list_bufs()
+              end,
+            },
+          },
+        },
+      })
+
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+          { name = "tags" },
+          { name = "buffer" },
+          { name = "rg",     option = { pattern = [[[\w_-]{5,60}]] } },
+        }),
+      })
+
+      require("luasnip").filetype_extend("ruby", { "rails" })
+      require("luasnip.loaders.from_vscode").lazy_load()
     end,
   },
 })
